1c32ec57306fcc8528e514916c8e19fcb980c639
GCharles85 <gcharles345@gmail.com>
2024-11-18 04:13:17 +0000

    add cors support and update exec(wget...) to exec(/usr/bin/wget/...)

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const { exec } = require('child_process');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 4000;
+
+/* -------- CORS Support -------- */
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.get('/fetch-rss', (req, res) => {
   const rssFeedUrl = 'https://www.womenshealthmag.com/rss/all.xml';
   const outputFile = 'rss_feed.xml';
 
   // Use wget to fetch the RSS feed
-  exec(`wget -O ${outputFile} ${rssFeedUrl}`, (error, stdout, stderr) => {
+  exec(`/usr/bin/wget -O ${outputFile} ${rssFeedUrl}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing wget: ${error}`);
       res.status(500).send('Error fetching RSS feed');
@@ -23,7 +31,7 @@ app.get('/fetch-rss', (req, res) => {
         res.status(500).send('Error reading RSS feed');
         return;
       }
-
+      console.log(data);
       res.send(data);
 
       // Optionally delete the file after reading
@@ -36,6 +44,6 @@ app.get('/fetch-rss', (req, res) => {
   });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0',() => {
   console.log(`Server running on http://localhost:${port}`);
 });
